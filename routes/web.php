baf013db8ca1e6450914ea40d62aa00692e74b36
sireler <sireler@gmail.com>
2018-08-26 16:37:12 +0300

    web routes

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,16 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// Chat
 Route::get('/chat', 'ChatController@index')->name('chat');
 Route::post('/chat', 'ChatController@postMessage')->name('sendMessage');
 Route::get('/chat/load', 'ChatController@loadMessages');
 
+// User
 Route::get('/profile', 'UserController@profile')->name('profile');
 Route::get('/user/info', 'UserController@getUserInfo');
 Route::post('/profile/avatar', 'UserController@changeAvatar')->name('changeAvatar');
 
+
+// Socket
 Route::get('/socket/check-auth', 'SocketController@checkAuth')->name('socket.check_auth');
