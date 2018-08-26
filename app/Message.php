57461df80823444f85e2be69ea9969a76d349970
sireler <sireler@gmail.com>
2018-08-26 16:41:57 +0300

    messages

--- app/Message.php
+++ app/Message.php
@@ -8,11 +8,21 @@ class Message extends Model
 {
     protected $fillable = ['from_id', 'to_id', 'content'];
 
+    /**
+     * From User instance
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function fromUser()
     {
         return $this->belongsTo('App\User', 'from_id', 'id');
     }
 
+    /**
+     * To User instance
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function toUser()
     {
         return $this->belongsTo('App\User', 'to_id', 'id');
